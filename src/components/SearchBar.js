a7330dd5184e4cb3fc646a09c1919edc45dec5de
anniemcmahon <anniemcmahon20@gmail.com>
2023-07-01 23:36:18 -0400

    added sort options event listener

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './SearchBar.css';
 
 function SearchBar() {
+  const [location, setLocation] = useState(null);
+  const [keyword, setKeyword] = useState(null);
+  const [sort, setSort] = useState("");
+
+  const sortChoices = [
+    {id: "best_match", choice: "Best Match"},
+    {id: "rating", choice: "Highest Rated"},
+    {id: "review_count", choice: "Most Reviewed"}
+  ];
+
   return (
     <div id="search-bar">
       <ul id="sort-options">
-        <li key="best_match" className="sort-choice">Best Match</li>
-        <li key="rating" className="sort-choice">Highest Rated</li>
-        <li key="review_count" className="sort-choice">Most Reviewed</li>
+        {sortChoices.map((choice) => (
+         <li key={choice.id} value={choice.id} onClick={() => setSort(choice.id)} className={sort == choice.id? "active" : ""}>{choice.choice}</li>
+        ))}
       </ul>
       <hr />
       <div id="user-input">
