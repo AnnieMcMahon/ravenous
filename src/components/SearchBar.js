c2f83b8f1ac243306e918d3a6388454404916665
anniemcmahon <anniemcmahon20@gmail.com>
2023-07-01 23:49:47 -0400

    added event listeners for keyword and location

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 
 function SearchBar() {
@@ -12,18 +12,30 @@ function SearchBar() {
     {id: "review_count", choice: "Most Reviewed"}
   ];
 
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value);
+  };
+
+  const handleKeywordChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log(`Location: ${location} Keyword: ${keyword}`);
+  };
+
   return (
     <div id="search-bar">
       <ul id="sort-options">
         {sortChoices.map((choice) => (
-         <li key={choice.id} value={choice.id} onClick={() => setSort(choice.id)} className={sort == choice.id? "active" : ""}>{choice.choice}</li>
+         <li key={choice.id} value={choice.id} onClick={() => setSort(choice.id)} className={sort === choice.id? "active" : ""}>{choice.choice}</li>
         ))}
       </ul>
       <hr />
       <div id="user-input">
-        <input className="search-input" id="location" placeholder="Search by location" />
-        <input className="search-input" id="keyword" placeholder="Search by keyword" />
-        <button id="submit" type="submit">Let's go!</button>
+        <input className="search-input" id="location" onChange={handleLocationChange} placeholder="Search by location" />
+        <input className="search-input" id="keyword" onChange={handleKeywordChange} placeholder="Search by keyword" />
+        <button id="submit" type="submit" onClick={handleSubmit}>Let's go!</button>
       </div>
     </div>
   );
