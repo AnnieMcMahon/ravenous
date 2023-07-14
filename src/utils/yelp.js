ad2edee91c63d337ef569b38c158a853ee4ce59b
anniemcmahon <anniemcmahon20@gmail.com>
2023-07-14 10:11:47 -0400

    fixed small issue

--- src/utils/yelp.js
+++ src/utils/yelp.js
@@ -1,4 +1,4 @@
-const apiKey = "GG-42_-ygrjiqwls0Om_dcANAyf2jUA5E5hVcJ7NBXXFGS-4yiArK9qEhbr7rmXrd1m6GG5exQMUQvIylqWabmaYzQOkIHo76YvqbzKDObeyiuWGM1daX3o46GanZHYx";
+const apiKey = API_KEY;
 const cors = "https://cors-anywhere.herokuapp.com/";
 const baseUrl = "https://api.yelp.com/v3";
 const searchEndpoint = "/businesses/search";
